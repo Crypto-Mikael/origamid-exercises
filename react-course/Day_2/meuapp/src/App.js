0459f5c23d5e0264af70e60f9aac86d585c6ce8e
Hamander <1hamander@gmail.com>
2021-07-24 15:34:18 -0300

    Dia 4 Finalizado

--- react-course/Day_2/meuapp/src/App.js
+++ react-course/Day_2/meuapp/src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import * as data from './data'
+import Footer from './components/Footer';
+import Header from './components/Header';
 function App() {
-  const dados = data.luana;
-
-  const sumValues = (data) => {
-    return data.compras.map((itens) => 
-      Number(itens.preco.split(" ")[1]))
-      .reduce((prev, curr) => prev + curr)
-  }
-
-  const statusValue = (boolean) => {
-    return boolean ? 'Ativa' : 'Inativa';
-  }
   return (
-    <> 
-     <h3>{`Nome: ${dados.cliente}`}</h3>
-     <h3>{`Idade: ${dados.idade}`}</h3>
-     Situação:{' '}
-     <span style={{ color: dados.ativa ? 'green' : 'red' }}>{statusValue(dados.ativa)}</span>
-     <h3>{`Total gasto: R$ ${sumValues(dados)}`}</h3>
-     { statusValue(dados.ativa) === 'Inativa' ? <p>Você está gastando demais</p> : null}
+    <>
+    <Header />
+    <Footer />
     </>
   );
 }
